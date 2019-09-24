beac3a038267c12520115d8edd0c9e82016a8b30
Part1nax777 <serewka-ru@yandex.ru>
2019-09-25 01:13:35 +0300

    fix feature tests

--- spec/features/user_views_graph_page_spec.rb
+++ spec/features/user_views_graph_page_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 feature "User view graph links" do
   scenario 'successfully' do
     visit root_path
-    expect(page).to have_button('BTC')
-    expect(page).to have_button('ETH')
-    expect(page).to have_button('Nasdaq')
+    expect(page).to have_link('Bitcoin')
+    expect(page).to have_link('Ethereum')
+    expect(page).to have_link('Nasdaq')
   end
 
   scenario 'visit to wiki' do
     visit currency_path("Bitcoin")
     expect(page).to have_link('Bitcoin', :href => "https://en.wikipedia.org/wiki/Bitcoin")
-    expect(page).to have_link('Etherium', :href => "https://en.wikipedia.org/wiki/Ethereum")
+    visit currency_path("Ethereum")
+    expect(page).to have_link('Ethereum', :href => "https://en.wikipedia.org/wiki/Ethereum")
+    visit currency_path("Nasdaq")
     expect(page).to have_link('Nasdaq', :href => "https://en.wikipedia.org/wiki/Nasdaq")
   end
 end
